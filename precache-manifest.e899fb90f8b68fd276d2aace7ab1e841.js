892fc3ae1bd12c0e577b3a7c78946374bb8ca234
Thiago Cruz <thiago2096@live.com>
2020-10-19 20:35:05 -0400

    Updates

---
+++ precache-manifest.e899fb90f8b68fd276d2aace7ab1e841.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6881a37558a40d2cd4e68e2f7c75467",
+    "url": "/belo-traje/index.html"
+  },
+  {
+    "revision": "b10e565de93d98b07a1e",
+    "url": "/belo-traje/static/css/2.c0e56554.chunk.css"
+  },
+  {
+    "revision": "f064b98812c5b27669cb",
+    "url": "/belo-traje/static/css/main.dc817c27.chunk.css"
+  },
+  {
+    "revision": "b10e565de93d98b07a1e",
+    "url": "/belo-traje/static/js/2.c9b1a281.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/belo-traje/static/js/2.c9b1a281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f064b98812c5b27669cb",
+    "url": "/belo-traje/static/js/main.230b6d19.chunk.js"
+  },
+  {
+    "revision": "a33bfba1134297b38887",
+    "url": "/belo-traje/static/js/runtime-main.afe76788.js"
+  },
+  {
+    "revision": "fa73c8628a3366e1f209095f34b9cb4e",
+    "url": "/belo-traje/static/media/item1.fa73c862.png"
+  },
+  {
+    "revision": "b16e67ed6fffcfc9a9e6140030b376fc",
+    "url": "/belo-traje/static/media/item10.b16e67ed.png"
+  },
+  {
+    "revision": "784a334154264147b0a577031d23df29",
+    "url": "/belo-traje/static/media/item11.784a3341.png"
+  },
+  {
+    "revision": "1c7745c796eacd4f28cc85caf0ff1e18",
+    "url": "/belo-traje/static/media/item12.1c7745c7.png"
+  },
+  {
+    "revision": "40b2ad7da92eeb1063221248e1e07fb8",
+    "url": "/belo-traje/static/media/item13.40b2ad7d.png"
+  },
+  {
+    "revision": "f0d66f103046b21cc9f64611e2d7c9fe",
+    "url": "/belo-traje/static/media/item14.f0d66f10.png"
+  },
+  {
+    "revision": "d47809993906cd0cddb2df036db7e44e",
+    "url": "/belo-traje/static/media/item15.d4780999.png"
+  },
+  {
+    "revision": "4e861363ab72103b015d2bce479d740d",
+    "url": "/belo-traje/static/media/item16.4e861363.png"
+  },
+  {
+    "revision": "b0f9278cf846d74b8c87d6bc5d18dc13",
+    "url": "/belo-traje/static/media/item17.b0f9278c.png"
+  },
+  {
+    "revision": "1cca76505fcf78aa0f981ba38864f7f1",
+    "url": "/belo-traje/static/media/item18.1cca7650.png"
+  },
+  {
+    "revision": "cba72a674777e8f44104fbb098cd61ba",
+    "url": "/belo-traje/static/media/item19.cba72a67.png"
+  },
+  {
+    "revision": "6ebf4047e9c15d418c8619ec2c371dd1",
+    "url": "/belo-traje/static/media/item2.6ebf4047.png"
+  },
+  {
+    "revision": "f4fee96971f8d09641308d406b31f81e",
+    "url": "/belo-traje/static/media/item3.f4fee969.png"
+  },
+  {
+    "revision": "48f89507d1bdfbfe2750f96e578e37e3",
+    "url": "/belo-traje/static/media/item4.48f89507.png"
+  },
+  {
+    "revision": "b0af20c6fe1bb12fc7902edb2faff3e9",
+    "url": "/belo-traje/static/media/item5.b0af20c6.png"
+  },
+  {
+    "revision": "c3c2bc9974a3b8a390dffe1b8b219b23",
+    "url": "/belo-traje/static/media/item6.c3c2bc99.png"
+  },
+  {
+    "revision": "d3091ebdcf970165c239a0ea03636913",
+    "url": "/belo-traje/static/media/item7.d3091ebd.png"
+  },
+  {
+    "revision": "ccbf1cedf3720d45dec6e52b361aeb5d",
+    "url": "/belo-traje/static/media/item8.ccbf1ced.png"
+  },
+  {
+    "revision": "f9355a2208692f8591f35fd002ad0715",
+    "url": "/belo-traje/static/media/item9.f9355a22.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/belo-traje/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/belo-traje/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/belo-traje/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/belo-traje/static/media/slick.f97e3bbf.svg"
+  }
+]);
